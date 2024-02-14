76e5977204d05c5bc26d453ba2656f53e8d868d8
Gor-Hoveyan <gorpro2004@mail.ru>
2024-02-15 03:52:47 +0400

    End of testing

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,7 +1,7 @@
 
 
 async function login(userName: string, password: string) {
-    const res = await fetch(`https://todo-with-auth-six.vercel.app/api/auth/login`, {
+    const res = await fetch(`http://localhost:3000/api/auth/login`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
